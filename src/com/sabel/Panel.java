4f833e5f9ddea9d33c5e5a7b2a9427cf5595f7c6
knoten <philipp.schweiger85@googlemail.com>
2017-10-07 10:39:37 +0200

    Implementet all Buttons with Background Color Change Function

--- src/com/sabel/Panel.java
+++ src/com/sabel/Panel.java
@@ -3,11 +3,21 @@ package com.sabel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.awt.Color.BLUE;
 
 public class Panel extends JFrame {
 
     JPanel jPW, jPC, jPS;
     ButtonGroup bg;
+    JRadioButton rbGruen;
+    JRadioButton rbBlau;
+    JRadioButton rbRot;
+    JButton jbGruen;
+    JButton jbBlau;
+    JButton jbRot;
 
 
     public Panel() {
@@ -17,7 +27,7 @@ public class Panel extends JFrame {
         this.setSize(300, 200);
 
         this.initComponents();
-//        this.initEvents();
+        this.initEvents();
 
         this.setVisible(true);
     }
@@ -29,17 +39,28 @@ public class Panel extends JFrame {
 
         bg = new ButtonGroup();
 
-        JRadioButton rbGruen = new JRadioButton("Gruen");
-        JRadioButton rbBlau = new JRadioButton("Blau");
-        JRadioButton rbRot = new JRadioButton("Rot");
+        rbGruen = new JRadioButton("Gruen");
+        rbBlau = new JRadioButton("Blau");
+        rbRot = new JRadioButton("Rot");
+
+        jbGruen = new JButton("Gruen");
+        jbBlau = new JButton("Blau");
+        jbRot = new JButton("Rot");
 
         rbBlau.setSelected(true);
         rbGruen.setSelected(true);
         rbRot.setSelected(true);
 
+        jbBlau.setSelected(true);
+        jbGruen.setSelected(true);
+        jbRot.setSelected(true);
+
         bg.add(rbBlau);
         bg.add(rbGruen);
         bg.add(rbRot);
+        bg.add(jbBlau);
+        bg.add(jbGruen);
+        bg.add(jbRot);
 
         jPW.setLayout(new BoxLayout(jPW,BoxLayout.Y_AXIS));
 
@@ -47,10 +68,65 @@ public class Panel extends JFrame {
         jPW.add(rbGruen);
         jPW.add(rbRot);
 
+        jPS.add(jbBlau);
+        jPS.add(jbGruen);
+        jPS.add(jbRot);
+
         this.add(jPC);
         this.add(jPW, BorderLayout.WEST);
         this.add(jPS, BorderLayout.SOUTH);
 
+    }
+
+    private void initEvents(){
+        jbBlau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPC.setBackground(Color.BLUE);
+                jbBlau.setSelected(true);
+                rbBlau.setSelected(true);
+            }
+        });
+        rbBlau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPC.setBackground(Color.BLUE);
+                jbBlau.setSelected(true);
+                rbBlau.setSelected(true);
+            }
+        });
+        jbGruen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPC.setBackground(Color.GREEN);
+                jbGruen.setSelected(true);
+                rbGruen.setSelected(true);
+            }
+        });
+        rbGruen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPC.setBackground(Color.GREEN);
+                jbGruen.setSelected(true);
+                rbGruen.setSelected(true);
+            }
+        });
+        jbRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPC.setBackground(Color.RED);
+                jbRot.setSelected(true);
+                rbRot.setSelected(true);
+            }
+        });
+        rbRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPC.setBackground(Color.RED);
+                jbRot.setSelected(true);
+                rbRot.setSelected(true);
+            }
+        });
     }
 
 
